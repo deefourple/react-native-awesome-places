f95c12c14270e58c43498045766707dca6d3b17f
Dave Dehghani <davedehghani@gmail.com>
2018-10-27 21:38:12 -0400

    added all the reducer logic necessary for each of the action types

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,6 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+import phImage from "../../assets/japan.jpg";
+
 const initialState = {
   placeName : '',
   places : [],
@@ -6,6 +9,32 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case ADD_PLACE:
+      return {
+          ...state,
+          places : state.places.concat({
+            name : action.placeName,
+            key : Math.random().toString(),
+            image : phImage
+          }),
+          placeName : '',
+      };
+    case DELETE_PLACE :
+      return {
+          ...state,
+        places : state.places.filter(place => place.key !== state.placeSelected.key),
+        selectedPlace : null
+      };
+    case SELECT_PLACE :
+      return {
+          ...state,
+        placeSelected : state.places.find(place => place.key === action.placeKey)
+      };
+    case DESELECT_PLACE :
+      return {
+          ...state,
+        selectedPlace : null
+      }
     default :
       return state;
   }
