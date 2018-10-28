db7590377083d911ea576d3c5296325834bfa142
Dave Dehghani <davedehghani@gmail.com>
2018-10-28 19:26:21 -0400

    created a directory for reusbale UI components created a reusable DefaultInput component and used it on the Auth Screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput';
 
 class AuthScreen extends Component {
-  loginHandler = () => {
-    startMainTabs();
-  };
+  loginHandler = () => startMainTabs();
+
 
   render() {
     return (
-        <View>
-          <Text>Auth Screen</Text>
-          <Button title="Login" onPress={this.loginHandler}/>
+        <View style={styles.container}>
+          <Text>Please Log In</Text>
+          <Button title="Switch to Login" onPress={this.loginHandler}/>
+          <View style={styles.inputContainer}>
+            <DefaultInput placeholder="Your E-Mail Address" />
+            <DefaultInput placeholder="Password" />
+            <DefaultInput placeholder="Confirm Password" />
+          </View>
+          <Button title="Submit" onPress={this.loginHandler}/>
         </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer : {
+    width: '80%',
+  },
+});
 
 export default AuthScreen;
