e9f15bc8f193a1142e658196342175ace2d83633
Dave Dehghani <davedehghani@gmail.com>
2018-10-27 21:19:30 -0400

    installed redux + react-redux to the app's dependencies set up all the action types and the required props needed to be passed into the reducers (if necessary)

---
+++ src/store/reducers/places.js
@@ -0,0 +1,14 @@
+const initialState = {
+  placeName : '',
+  places : [],
+  placeSelected : null
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default :
+      return state;
+  }
+};
+
+export default reducer;
