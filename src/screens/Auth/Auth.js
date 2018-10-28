a6f044409de2541ab74d7c31c79b31d6334601e1
Dave Dehghani <davedehghani@gmail.com>
2018-10-28 19:48:01 -0400

    created a reusbale UI Component for Wrapping Text components so that they can have default style properties such as colors, fontSize, fontWeight etc

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -4,6 +4,7 @@ import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText';
+import MainText from '../../components/UI/MainText';
 
 class AuthScreen extends Component {
   loginHandler = () => startMainTabs();
@@ -12,7 +13,9 @@ class AuthScreen extends Component {
   render() {
     return (
         <View style={styles.container}>
-          <HeadingText>Please Log In</HeadingText>
+          <MainText>
+            <HeadingText>Please Log In</HeadingText>
+          </MainText>
           <Button title="Switch to Login" onPress={this.loginHandler}/>
           <View style={styles.inputContainer}>
             <DefaultInput
